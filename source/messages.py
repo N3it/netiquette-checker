622371322fb75097430e12b4d5a8f12e56150981
Sébastien Delsart <sebastien.delsart@epita.fr>
2020-09-17 20:03:32 +0200

    source: errors functions transfered to messages.py

---
+++ source/messages.py
@@ -0,0 +1,49 @@
+class Messages:
+
+    def __init__(self):
+        # True if an occurs else False
+        self.error = False
+        # Colors
+        self.SUCCESS = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+
+
+    def success(self):
+        print(f"{self.SUCCESS}[OK] Your message not contain any syntax errors")
+
+
+    def error_tags(self):
+        print(f"{self.FAIL}[X] Your tags must respect the following rules:\n"
+                "    tag-id = 1*(UPPER / DIGIT / \"-\", \"_\" / \"+\" / \"/\")\n"
+                "    tags = 2( \"[\" tag-id \"]\" )")
+        self.error = 1
+
+
+    def error_signature(self):
+        print(f"{self.FAIL}[X] Your message must have a signature. "
+                "A signature starts with \"--\" SPACE CRLF")
+        self.error = 1
+
+
+    def error_courtesy(self):
+        print(f"{self.FAIL}[X] Missing or incorrect common courtesy.")
+        self.error = 1
+
+
+    def error_trailing_whitespaces(self, line_id):
+        print(f"{self.FAIL}[X] Trailing whitespaces must not be used on line", 
+                line_id+1)
+        self.error = 1
+
+
+    def error_max_length_subject(self):
+        print(f"{self.FAIL}[X] The length of your subject must not exceed 80 "
+                "characters.")
+        self.error = 1
+
+    def error_max_length_message(self, line_id):
+        print(f"{self.FAIL}[X] The length of the line", line_id+1, "must not "
+                "exceed 72 characters.")
+        self.error = 1
--- source/netiquette-checker.py
+++ source/netiquette-checker.py
@@ -1,64 +1,20 @@
 import sys
 import re
-
-class colors:
-    SUCCESS = '\033[92m'
-    WARNING = '\033[93m'
-    ERROR = '\033[91m'
-    ENDC = '\033[0m'
+from messages import Messages
 
 TAGS_REGEX = re.compile(r"^(\[[A-Z0-9-_+/]*\])*$")
-ERROR = 0
+messages = Messages()
 
 def help():
     print("USAGE: python3", sys.argv[0], "SUBJECT FILE")
     sys.exit()
 
 
-def success():
-    print(f"{colors.SUCCESS}[OK] Your message not contain any syntax errors")
-
-
-def error_tags():
-    print(f"{colors.ERROR}[X] Your tags must respect the following rules:\n"
-          "    tag-id = 1*(UPPER / DIGIT / \"-\", \"_\" / \"+\" / \"/\")\n"
-          "    tags = 2( \"[\" tag-id \"]\" )")
-    ERROR = 1
-
-
-def error_signature():
-    print(f"{colors.ERROR}[X] Your message must have a signature. "
-           "A signature starts with \"--\" SPACE CRLF")
-    ERROR = 1
-
-
-def error_courtesy():
-    print(f"{colors.ERROR}[X] Missing or incorrect common courtesy.")
-    ERROR = 1
-
-
-def error_trailing_whitespaces(line_id):
-    print(f"{colors.ERROR}[X] Trailing whitespaces must not be used on line", 
-            line_id+1)
-    ERROR = 1
-
-
-def error_max_length_subject():
-    print(f"{colors.ERROR}[X] The length of your subject must not exceed 80 "
-           "characters.")
-    ERROR = 1
-
-def error_max_length_message(line_id):
-    print(f"{colors.ERROR}[X] The length of the line", line_id+1, "must not "
-           "exceed 72 characters.")
-    ERROR = 1
-
-
 def check_trailing_whitespaces(s):
     l = s.split("\n")
     for line in l:
         if len(line) > 0 and line[-1] == " " and line != "-- ":
-            error_trailing_whitespaces(line)
+            messages.error_trailing_whitespaces(line)
 
 
 def parse_subject(subject):
@@ -69,12 +25,12 @@ def parse_subject(subject):
 
 def check_tags(tags, summary):
     if not re.fullmatch(TAGS_REGEX, tags):
-        error_tags() 
+        messages.error_tags() 
 
 
 def check_subject(subject):
     if len(subject) > 80:
-        error_max_length_subject()
+        messages.error_max_length_subject()
     tags, summary = parse_subject(subject)
     check_tags(tags, summary)
     check_trailing_whitespaces(subject)
@@ -91,7 +47,7 @@ def check_max_length_message(lines):
     for i in range(len(lines)):
         line = lines[i]
         if len(line) > 72:
-            error_max_length_message(i)
+            messages.error_max_length_message(i)
 
 
 def check_signature(lines):
@@ -99,21 +55,21 @@ def check_signature(lines):
         i = lines.index("-- ")
         return i
     except:
-        error_signature()
+        messages.error_signature()
         return -1
 
 
 def check_courtesy(lines, signature_index):
     if signature_index == -1:
-        error_courtesy()
+        messages.error_courtesy()
     else:
         i = signature_index - 2 
         greet, salutations = lines[0], lines[i]
         if greet[-1] != "," or lines[1] != "":
-            error_courtesy()
+            messages.error_courtesy()
         if (len(salutations) < 2 or salutations[-1] != "," 
             or lines[i-1] != "" or lines[i+1] != ""):
-            error_courtesy()
+            messages.error_courtesy()
 
 
 def check_message(message):
@@ -134,8 +90,8 @@ def main():
 
         check_subject(subject)
         check_message(message)
-        if ERROR == 0:
-            success()
+        if not messages.error:
+            messages.success()
 
 
 if __name__ == "__main__":
